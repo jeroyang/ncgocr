181fc2e3dba7ce76be0b7a31945ae82b94804c69
jeroyang <jeroyang@gmail.com>
2017-08-01 01:01:59 +0800

    simplify import

--- mcgocr/__init__.py
+++ mcgocr/__init__.py
@@ -8,10 +8,8 @@ from builtins import *
 import os
 __author__ = 'Chia-Jung, Yang'
 __email__ = 'jeroyang@gmail.com'
-__version__ = '0.1.0'
 
 from mcgocr.concept import *
 from mcgocr.corpus_craft import *
-from mcgocr.extractor import *
-from mcgocr.learning import *
 from mcgocr.pattern_regex import *
+from mcgocr.mcgocr import *
